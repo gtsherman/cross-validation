bc788376b9e98ff0b5cb0deb2648ad2dc0872691
Garrick Sherman <gsherma2@illinois.edu>
2017-08-23 16:27:52 -0700

    run is more modular to allow easier extending

--- run.py
+++ run.py
@@ -13,6 +13,50 @@ formats = {
 }
 
 def main(args):
+    reader = load_data(args)
+
+    # Run cross-validation
+    validator = KFoldValidator(num_folds=args.folds, verbose=args.verbose)
+    scored_test_items = validator.cross_validate(*reader.scored_items(), seed=args.seed)
+
+    # Output results
+    for item in scored_test_items:
+        print('{}\t{}'.format(item, str(scored_test_items[item])))
+    if args.summarize:
+        print('all\t{}'.format(str(validator.summarize(scored_test_items))))
+
+
+def get_args(**extra_args):
+    """
+    Get command line options necessary to run most cross-validation.
+    :param extra_args: Supply each extra argument with all its argparse options as a dict; the argument name is the flag
+    :return: The parsed command line arguments
+    """
+    parser = argparse.ArgumentParser(description='Run cross-validation.')
+    parser.add_argument('-f', '--file', action='append', help='a file representing an item to be included in '
+                                                              'cross-validation; either this or -d must be specified')
+    parser.add_argument('-d', '--directory', action='append', help='a directory of files, each of which is an item to '
+                                                                   'be included in cross-validation; either this or '
+                                                                   '-f must be specified')
+    parser.add_argument('-k', '--folds', help='number of folds to use in cross-validation', default=10)
+    parser.add_argument('-r', '--seed', help='set the random seed for item shuffling', default=random.random())
+    parser.add_argument('-s', '--summarize', action='store_true', help='include summary statistic')
+    parser.add_argument('-v', '--verbose', action='store_true', help='verbose output')
+    parser.add_argument('-m', '--metric', help='the metric to optimize for cross-validation; required for some input '
+                                               'formats')
+    parser.add_argument('-i', '--input-format', choices=formats.keys(), help='input file format', default='trec')
+
+    if extra_args:
+        for extra_arg in extra_args:
+            flag = extra_arg.replace('_', '-')
+            parser.add_argument('--{}'.format(flag), **extra_args[extra_arg])
+
+    args = parser.parse_args()
+
+    return args
+
+
+def load_data(args):
     if args.input_format == 'trec' and not args.metric:
         print('Must specify metric for this input format.')
         exit()
@@ -33,30 +77,9 @@ def main(args):
     for file in files:
         reader.read(file, args.metric)
 
-    # Run cross-validation
-    validator = KFoldValidator(args.folds, verbose=args.verbose)
-    scored_test_items = validator.cross_validate(*reader.scored_items(), seed=args.seed)
+    return reader
 
-    # Output results
-    for item in scored_test_items:
-        print('{}\t{}'.format(item, str(scored_test_items[item])))
-    if args.summarize:
-        print('all\t{}'.format(str(validator.summarize(scored_test_items))))
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Run cross-validation.')
-    parser.add_argument('-f', '--file', action='append', help='a file representing an item to be included in '
-                                                              'cross-validation; either this or -d must be specified')
-    parser.add_argument('-d', '--directory', action='append', help='a directory of files, each of which is an item to '
-                                                                   'be included in cross-validation; either this or '
-                                                                   '-f must be specified')
-    parser.add_argument('-k', '--folds', help='number of folds to use in cross-validation', default=10)
-    parser.add_argument('-r', '--seed', help='set the random seed for item shuffling', default=random.random())
-    parser.add_argument('-s', '--summarize', action='store_true', help='include summary statistic')
-    parser.add_argument('-v', '--verbose', action='store_true', help='verbose output')
-    parser.add_argument('-m', '--metric', help='the metric to optimize for cross-validation; required for some input '
-                                               'formats')
-    parser.add_argument('-i', '--input-format', choices=formats.keys(), help='input file format', default='trec')
-    args = parser.parse_args()
-
+    args = get_args()
     main(args)
