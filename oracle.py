bf1aebcae2a05d61dffe8c850e388031c20e43d2
Garrick Sherman <gsherma2@illinois.edu>
2017-12-11 17:15:03 -0800

    added oracle runner

---
+++ oracle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import statistics
+
+from cross_validation import KFoldValidator
+from run import get_args, load_data
+
+
+def main(args):
+    reader = load_data(args)
+
+    # Run oracle
+    scores = []
+    validator = KFoldValidator(num_folds=args.folds, verbose=args.verbose)
+    for item in reader.scored_items():
+        best_params, best_params_score = validator.train([item])
+        scores.append(best_params_score)
+        if args.verbose:
+            print(item.name, best_params, best_params_score, sep='\t')
+
+    if args.summarize:
+        print('all', '---', statistics.mean(scores), sep='\t')
+
+
+if __name__ == '__main__':
+    args = get_args()
+    main(args)
